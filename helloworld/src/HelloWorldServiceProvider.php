e1b4c10116a2924207a853e8a195a1468bd5ee3e
dungnv <dungnv@asiantech.vn>
2017-05-04 14:33:37 +0700

    update

---
+++ helloworld/src/HelloWorldServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dungnv\Helloworld;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelloWorldServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['dungnv'] = $this->app->share(function($app) {
+          return new Helloworld;
+        });
+    }
+}
---
+++ helloworld/src/Helloworld.php
@@ -0,0 +1,9 @@
+<?php
+namespace Dungnv\Helloworld;
+ 
+class Helloworld {
+ 
+    public static function sayHello() {
+        return 'Hello World! I am DungNV';
+    }
+}
